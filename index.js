d41e4d7841ac2fb7997255d154fa20e45a285d67
shohan11d <shohan11d@gmail.com>
2026-05-24 13:19:38 +0600

    52-3 Add Destination Form and create Post API

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const dns = require("node:dns");
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require('cors');
 const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
-
-const uri = process.env.MONGODB_URI;
-
 const app = express();
+const uri = process.env.MONGODB_URI;
 const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -23,16 +25,23 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+
+    const db = client.db("wanderlast");
+    const destinationsCollection = db.collection("destinations");
+
+    app.post("/destination", async (req, res) => {
+      const destination = req.body;
+      console.log("destination", destination);
+      const result = await destinationsCollection.insertOne(destination);
+      res.json(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
     );
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
   }
 }
 run().catch(console.dir);
