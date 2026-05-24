f200c3a69f526a9b3b822da0aba0b743aff975c3
shohan11d <shohan11d@gmail.com>
2026-05-24 17:49:05 +0600

    52-7 Delete Destination

--- index.js
+++ index.js
@@ -61,6 +61,14 @@ async function run() {
       res.json(result);
     });
 
+    app.delete("/destination/:id", async (req, res) => {
+      const { id } = req.params;
+      const result = await destinationsCollection.deleteOne({
+        _id: new ObjectId(id),
+      });
+      res.json(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
