a8d2632a70cbfc4e4374de6799f12f1946903239
shohan11d <shohan11d@gmail.com>
2026-05-24 17:28:10 +0600

    52-6 Edit Destination

--- index.js
+++ index.js
@@ -50,6 +50,17 @@ async function run() {
       res.json(result);
     });
 
+    app.patch("/destination/:id", async (req, res) => {
+      const { id } = req.params;
+      const updatedData = req.body;
+      console.log(updatedData);
+      const result = await destinationsCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: updatedData },
+      );
+      res.json(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
