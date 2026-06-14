bd21390c0364e01c1942165e38988881a4d6e7ac
shohan11d <shohan11d@gmail.com>
2026-06-14 20:52:46 +0600

    jwt remvoed

--- index.js
+++ index.js
@@ -24,9 +24,6 @@ const client = new MongoClient(uri, {
   },
 });
 
-const JWKS = createRemoteJWKSet(
-  new URL(`${process.env.CLIENT_URL}/api/auth/jwks`),
-);
 
 const verifyToken = async (req, res, next) => {
   const authHeader = req?.headers.authorization;
