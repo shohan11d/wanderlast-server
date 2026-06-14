a8dc1d601653cb1561a3c9af0b8ae77792fe965e
shohan11d <shohan11d@gmail.com>
2026-06-14 20:52:11 +0600

    patch fixed

--- index.js
+++ index.js
@@ -79,7 +79,16 @@ async function run() {
       res.json(result);
     });
 
-   
+    app.patch("/tutorUpdate/:id", async (req, res) => {
+      const { id } = req.params;
+      const updatedData = req.body;
+      console.log(updatedData);
+      const result = await tutorsCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: updatedData },
+      );
+      res.json(result);
+    });
 
     app.patch("/tutor/:id", async (req, res) => {
       const id = req.params.id;
